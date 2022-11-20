f0bde5944df42272dceeb6989c774ea01cda5798
Koen-sanders <koensanders@outlook.com>
2022-11-20 21:13:50 +0100

    added repo to org

---
+++ AllDailyDuties-Gateway/CustomJwtAuthExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AllDailyDuties_Gateway
+{
+    public static class CustomJwtAuthExtension
+    {
+        public static void AddCustomJwtAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("RXApQIdkg420Ceb3P1337HeaKHvC26Ws69k1u3W2M"))
+                };
+
+            });
+        }
+    }
+}
---
+++ AllDailyDuties-Gateway/Program.cs
@@ -0,0 +1,34 @@
+using AllDailyDuties_Gateway;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("Ocelot.json");
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddOcelot();
+builder.Services.AddCustomJwtAuthentication();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseOcelot().Wait();
+
+
+app.Run();
